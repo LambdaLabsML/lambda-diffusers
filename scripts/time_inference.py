0673aff270aa842c13f0e8848a8ce9b8db84d52b
eolecvk <eolecvk@gmail.com>
2022-09-27 14:33:59 -0700

    switch from timeit to torch.utils.benchmark.Timer.timeit

--- scripts/time_inference.py
+++ scripts/time_inference.py
@@ -1,35 +1,54 @@
+
+
 from diffusers import StableDiffusionPipeline
 import torch
 
-def setup_inference_pipeline():
-    pipe = StableDiffusionPipeline.from_pretrained("lambdalabs/sd-pokemon-diffusers", torch_dtype=torch.float16)  
-    pipe = pipe.to("cuda")
 
+def setup_inference_pipeline(dtype=torch.float16):
+    '''
+    returns HuggingFace diffuser pipeline
+    cf https://github.com/huggingface/diffusers#text-to-image-generation-with-stable-diffusion
+    note: could not download from CompVis/stable-diffusion-v1-4 (access restricted)
+    '''
+    from diffusers import StableDiffusionPipeline
+    pipe = StableDiffusionPipeline.from_pretrained(
+        "lambdalabs/sd-pokemon-diffusers",
+        torch_dtype=dtype
+    )
+    pipe = pipe.to("cuda")
     # Disable safety
     disable_safety = True
     if disable_safety:
         def null_safety(images, **kwargs):
             return images, False
         pipe.safety_checker = null_safety
-    
     return pipe
 
 
-def do_inference(pipe, n_samples=1, guidance_scale=3.0):
-    prompt = "Lincoln"
+def do_inference(pipe):
+    prompt = "a photo of an astronaut riding a horse on mars"
     with torch.autocast("cuda"):
-        images = pipe(n_samples*[prompt], guidance_scale=guidance_scale).images
-    return images
+        image = pipe(prompt).images[0]
+        # n_samples=1, guidance_scale=3.0
+        # images = pipe(n_samples*[prompt], guidance_scale=guidance_scale).images # -> batches ?
+        # image.save("astronaut_rides_horse.png")
+    return image
 
 
-def get_inference_timing():
-    import timeit
-    setup="from __main__ import do_inference,setup_inference_pipeline; pipe=setup_inference_pipeline()"
-    time_loop="do_inference(pipe)"
-    min_loop_runtime = min(timeit.repeat(time_loop, setup=setup, number=1, repeat=3))
-    return min_loop_runtime
+def get_inference_timing(n_repeats=2):
+    from torch.utils.benchmark import Timer
+    pipe = setup_inference_pipeline()
+    timer = Timer(
+        stmt="do_inference(pipe)",
+        setup="from __main__ import do_inference",
+        globals={"pipe": pipe},
+    )
+    profile_result = timer.timeit(n_repeats) # benchmark.Timer seems to performs 2 iterations for warmup
+    return f"Latency: {profile_result.mean * 1000:.5f} ms"
+
 
 
 if __name__ == "__main__":
 
-    print('Best time:', get_inference_timing())
+    r = get_inference_timing()
+    print(r)
